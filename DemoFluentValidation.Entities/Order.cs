9d35b4ec3d0cf0a5c6d58d41510d2d9c4392d93a
Sergi Ortiz Gómez <drualcman@msn.com>
2021-07-29 08:29:56 +0800

    add entity project

---
+++ DemoFluentValidation.Entities/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DemoFluentValidation.Entities
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public int CustomerId { get; set; }
+        public int EmployeeId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string ShipName { get; set; }
+        public string ShipAddress { get; set; }
+        public string ShipCity { get; set; }
+        public string ShipPostalCode { get; set; }
+        public List<OrderDetail> OrderDetails { get; set; }
+
+    }
+}
